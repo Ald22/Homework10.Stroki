d9b1af1d643584fa69da05783dc60f62e8aa47eb
Алдар <aldar.nanzitovich@gmail.com>
2023-03-17 16:53:04 +0800

    Финиш2

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,10 @@ public class Main {
         //Задача 1
         System.out.println("Задача 1");
 
-        String firstName = "Ivan ";
-        String middleName = "Ivanov ";
+        String firstName = "Ivan";
+        String middleName = "Ivanov";
         String lastName = "Ivanovich";
-        String fullName = middleName + firstName + lastName;
+        String fullName = middleName + " " + firstName + " " + lastName;
         System.out.println("ФИО сотрудника " + fullName);
 
         //Задача 2
@@ -19,10 +19,10 @@ public class Main {
 
         //Task3
         System.out.println("Task 3");
-        String middleName2 = "Иванов ";
-        String firstName2 = "Семён ";
+        String middleName2 = "Иванов";
+        String firstName2 = "Семён";
         String lastName2 = "Семёнович";
-        String fullName1 = middleName2 + firstName2 + lastName2;
+        String fullName1 = middleName2 + " " + firstName2 + " " + lastName2;
         String fullName3 = fullName1.replace('ё', 'е');
         System.out.println("Данные ФИО сотрудника — " + fullName3);
     }
